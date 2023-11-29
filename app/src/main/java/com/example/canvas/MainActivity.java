58737ba4ddbaef03ac8a77e293142176b21a82ee
cassie <cassiejc123@gmail.com>
2023-11-29 11:43:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.canvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout rlview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rlview = findViewById(R.id.rlView);
+        MyCanvas myCanvas = new MyCanvas(this);
+        rlview.addView(myCanvas);
+    }
+}
